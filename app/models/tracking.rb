7935dd5541a62f4dd24306eccb6e3d10d9f617a7
robmckinnon <rob@theyworkforyou.co.nz>
2008-08-07 14:21:15 +0100

    refactoring

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -8,43 +8,46 @@ class Tracking < ActiveRecord::Base
 
   before_validation_on_create :set_created_at_date, :default_booleans
 
-  def self.from_user_and_item user, item
-    if (item and user)
-      item_type = get_item_type item
-      Tracking.find_by_user_id_and_item_type_and_item_id user.id, item_type, item.id
-    else
-      nil
-    end
-  end
-
-  def self.all_for_item item, user=nil
-    item_type = get_item_type item
-    trackings = Tracking.find_all_by_item_id_and_item_type item.id, item_type
-    if user
-      trackings.sort! do |a,b|
-        if a.user_id == user.id
-          0
-        elsif b.user_id == user.id
-          1
-        else
-          a.user.login <=> b.user.login
-        end
+  class << self
+    def from_user_and_item user, item
+      if (item and user)
+        item_type = get_item_type item
+        find_by_user_id_and_item_type_and_item_id user.id, item_type, item.id
+      else
+        nil
       end
-    else
-      trackings.sort! { |a,b| a.user.login <=> b.user.login }
     end
-    trackings
+
+    def all_for_item item, user=nil
+      item_type = get_item_type item
+      trackings = find_all_by_item_id_and_item_type item.id, item_type
+      if user
+        trackings.sort! do |a,b|
+          if a.user_id == user.id
+            0
+          elsif b.user_id == user.id
+            1
+          else
+            a.user.login <=> b.user.login
+          end
+        end
+      else
+        trackings.sort! { |a,b| a.user.login <=> b.user.login }
+      end
+      trackings
+    end
+
+    protected
+      def get_item_type item
+        item_type = item.class
+        while (item_type.superclass != ActiveRecord::Base)
+          item_type = item_type.superclass
+        end
+        item_type.name
+      end
   end
 
   protected
-
-    def self.get_item_type item
-      item_type = item.class
-      while (item_type.superclass != ActiveRecord::Base)
-        item_type = item_type.superclass
-      end
-      item_type.name
-    end
 
     def set_created_at_date
       self.created_at = (Time.now.utc + TimeZone.new("Auckland").utc_offset).to_date
