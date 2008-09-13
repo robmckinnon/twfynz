08b14935c81b4ea21ac22af1ab20ad200be5e9fc
robmckinnon <rob@theyworkforyou.co.nz>
2008-09-13 18:34:27 +0100

    improved data loading time

--- db/migrate/044_add_date_int_to_contributions.rb
+++ db/migrate/044_add_date_int_to_contributions.rb
@@ -2,14 +2,42 @@ class AddDateIntToContributions < ActiveRecord::Migration
   def self.up
     add_column :contributions, :date_int, :integer
 
-    Contribution.find(:all, :include => :spoken_in).each do |contribution|
-      if contribution.spoken_in
-        contribution.update_attribue('date_int', contribution.spoken_in.date.to_s.tr('-','').to_i)
+    count = Contribution.count.to_f
+    group_size = 100
+    index = 0
+
+    start_timing
+
+    while (offset = index * group_size) < count
+      log_duration (offset / count) if index > 0
+      contributions = Contribution.find(:all, :offset=>offset, :limit=>group_size, :select=>'date, id')
+      contributions.each do |contribution|
+        contribution.update_attribute('date_int', contribution.date.to_s.tr('-','').to_i) if contribution.date
       end
+      index = index.next
     end
+
   end
 
   def self.down
     remove_column :contributions, :date_int
   end
+
+  def self.start_timing
+    @start = Time.now
+  end
+
+  def self.log_duration percentage_complete=nil
+    duration = Time.now - @start
+    estimated_time = (duration / percentage_complete)
+    estimated_remaining = ((estimated_time - duration) / 60).to_i
+    if estimated_remaining > 60
+      estimated_remaining = (estimated_remaining * 10 / 60) / 10.0
+      estimated_remaining = "#{estimated_remaining} hours"
+    else
+      estimated_remaining = "#{estimated_remaining} mins"
+    end
+    puts "remaining: #{estimated_remaining}"
+  end
+
 end
