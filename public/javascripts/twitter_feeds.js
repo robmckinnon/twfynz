9445c8c45e929da088cc99c69e9480c2c0438677
robmckinnon <rob@theyworkforyou.co.nz>
2009-04-04 02:31:54 +0100

    now use json formatted twitter feed

--- public/javascripts/twitter_feeds.js
+++ public/javascripts/twitter_feeds.js
@@ -1,74 +1,128 @@
-google.load("feeds", "1");
+var dateExp = new RegExp('([0-9][0-9][0-9][0-9])');
+var timeExp = new RegExp('([0-9][0-9][0-9][0-9])([0-9][0-9])([0-9][0-9])T([0-9][0-9])([0-9][0-9])([0-9][0-9]).*');
 
-function render_feed(dom_id, feed_title, url) {
-  var html = '  <div class="hfeed">\n'+
-            '    <h2 class="feed-title">' + feed_title + '</h2>\n'+
-            '    <div class="entries">\n';
-  if (results[dom_id].length > 0) {
-    for (var i = 0; i < results[dom_id].length; i++) {
-      var entry = results[dom_id][i];
+window.TimeIs = function() {;}
 
-      var content = entry.content;
-      var title = entry.title;
-      var author = content.split(':')[0];
-      var when = 'recently';
-      var publishedDate = entry.publishedDate;
-      var link = entry.link;
+window.TimeIs.prototype = {
+  get_local_time_for_date: function(time, alt) {
+    var system_date = Date.parse(time);
+    if (system_date) {
+      var user_date = new Date();
+      var delta_minutes = Math.floor((user_date - system_date) / (60 * 1000));
+      var distance = this.distance_of_time_in_words(delta_minutes);
+      if (delta_minutes < 0) {
+        return distance + " from now";
+      } else {
+        return distance + " ago";
+      }
+    } else {
+      return alt;
+    }
+  },
 
-      html += '      <div class="hentry">\n'+
-              '        <div class="entry-title"><a href="' + link + '">' + title + '</a></div>\n';
-      html += '      <div><span class="vcard author">' + author + '</span>';
-      html += ' <span class="published" title="' + publishedDate + '">' + when + '</span>';
+  // a vague copy of rails' inbuilt function,
+  // but a bit more friendly with the hours.
+  distance_of_time_in_words: function(minutes) {
+    if (minutes.isNaN) return "";
+    minutes = Math.abs(minutes);
+    if (minutes < 1) return 'less than a minute';
+    if (minutes < 50) return minutes.toString() + ' minute' + (minutes == 1 ? '' : 's');
+    if (minutes < 90) return 'about one hour';
+    if (minutes < 1080) return Math.round(minutes / 60).toString() + "#{0} hours";
+    if (minutes < 1440) return 'one day';
+    if (minutes < 2880) return 'about one day';
+    if (minutes > 525600) return 'over a year';
+    if (minutes > 43776) {
+      months = Math.round(minutes / 43776)
+      return months.toString() + " month" + (months == 1 ? '' : 's');
+    } else {
+      return Math.round(minutes / 1440).toString() + " days";
+    }
+  }
+}
 
-      html += '</div>\n'+
-              //'        <div class="entry-summary">' + content + '</div>\n'+
-              '      </div>\n';
+function hyperlink(txt) {
+  var regUrl = /(^|[^>\"\/])(http:\/\/|www\.)(?:[^\"])\S*([\s\)\!]|$)/gi;
+  var regUrlTail = /[\!\)\]\.\?]+$/g;
+  txt = txt.replace(regUrl, function($href,$start,$urlStart,$end,$pos,$txt) {
+    if(!$href) {
+      return '';
+    }
+    if($start) {
+      $href = $href.substr(1,$href.length-1);
+    }
+    if($end) {
+      $href = $href.substr(0,$href.length-1);
+    }
+    var trail = $href.match(regUrlTail);
+    if(trail) {
+      $href = $href.replace(regUrlTail,'');
+    }
+    if($href.search(/http/i)!=0) {
+      $href = 'http://' + $href;
+    }
+    var link = $start + '<a href="' + $href + '">' + $href.replace('http://','') + '</a>' + (trail?trail[0]:'') + $end;
+    return link; // add the start and trail+end back on
+  });
+  return txt;
+};
+
+function replace_all(text, old, replace) {
+  while(text.indexOf(old) > 0) { text = text.replace(old,replace); }
+  return text;
+}
+
+function render_feed(results, dom_id, feed_title) {
+  var entry;
+  var html =  '<div>';
+  if (results.length > 0) {
+    for (var i = 0; i < results.length; i++) {
+      entry = results[i];
+      var content = entry.text;
+      var title = hyperlink(content);
+      var author = entry.user.screen_name;
+      var publishedDate = entry.created_at;
+      var user_link = "http://twitter.com/" + author;
+      var link = user_link + "/statuses/" + entry.id;
+      var img = entry.user.profile_image_url;
+      var timeIs = new window.TimeIs();
+      var when = timeIs.get_local_time_for_date(publishedDate, publishedDate);
+
+      html += '  <div class="hentry">\n'+
+              '    <div class="entry-title">'+
+              '      <img style="border: 0; float: left; margin-right: 0.5em;" width="24" height="24" src="' + img + '"></img>'+
+              '      <span class="vcard author"><a href="'+user_link+'">' + author + '</a></span> ' +
+                     title +
+              '      <span class="published" title="' + publishedDate + '"><a href="' + link + '">' + when + '</a></span>'+
+              '    </div>\n';
+      html += '\n'+
+              '  </div>\n';
+              //'  <div class="entry-summary">' + content + '</div>\n'+
     }
 
-    html += '    </div>\n'+
-            '    <div class="acknowledgement">Tweets thanks to <a href="http://twitter.com/twfynz">Twitter and @twfynz</a>.</div>\n'+
-            '  </div>\n';
+    html +=   '</div>\n'+
+              '<div class="acknowledgement">Tweets thanks to <a href="http://twitter.com/twfynz">Twitter and @twfynz</a>.</div>';
   } else {
-    html += '      <p class="acknowledgement">No recent news items found with <a href="' + url + '">Google News Search</a>.</p>\n'+
-            '    </div>\n'+
-            '  </div>\n';
+    html +=   '</div>\n';
   }
 
   document.getElementById(dom_id).innerHTML = html;
 }
 
-function make_feed(dom_id, feed_title, urls) {
-  if (results[dom_id] == null) {
-    results[dom_id] = new Array;
-  }
-  counts[dom_id] = urls.length;
+function get_json() {
+  var url = "/friends_timeline.json";
 
-  for (var i = 0; i < urls.length; i++) {
-    var url = urls[i];
-    var feed = new google.feeds.Feed(url);
-    feed.setNumEntries(20);
-    feed.load(function(result) {
-      if (!result.error) {
-        var feed = result.feed;
-        for (var i = 0; i < feed.entries.length; i++) {
-          var entry = feed.entries[i];
-          var link = entry['link'];
-          results[dom_id].push(entry);
-        }
-
-        counts[dom_id] = counts[dom_id] - 1;
-        if (counts[dom_id] == 0) {
-          render_feed(dom_id, feed_title, url);
-        }
+  new Ajax.Request(url, {
+    method: 'get',
+    onSuccess: function(response) {
+      var status = replace_all(response.statusText,' ','');
+      if (status == 'OK') {
+        var json = response.responseText;
+        var data = eval('(' + json + ')');
+        render_feed(data, 'twitter_feed', "From Twitter")
       }
-    });
-  }
+    }
+  });
 }
 
-function initialize() {
-  results = new Object;
-  counts = new Object;
-  make_feed('twitter_feed', 'From Twitter', ["http://theyworkforyou.co.nz/friends_timeline.atom"]);
-}
-
-google.setOnLoadCallback(initialize);
+Event.observe(window, 'load', function() { get_json(); } );
