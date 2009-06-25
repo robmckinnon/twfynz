60c29b0f2c3c17cac4e303fbb13e5cc71feee654
robmckinnon <rob@theyworkforyou.co.nz>
2009-06-26 00:08:23 +0100

    loosened date req

--- app/models/persisted_file.rb
+++ app/models/persisted_file.rb
@@ -44,7 +44,7 @@ class PersistedFile < ActiveRecord::Base
     end
 
     def date_after_sept_2005? date
-      date.year > 2008 || (date.year == 2008 && date.month > 11)
+      date.year > 2008 || (date.year == 2008 && date.month > 08)
     end
 
     def load_debates publication_status, sleep_seconds
