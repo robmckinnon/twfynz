c93b9c344dd64af8d5ef40be7fe95ab6786cdd49
robmckinnon <rob@theyworkforyou.co.nz>
2009-08-24 11:08:20 +0100

    expire party vote comparisons cache on save

--- app/models/party_vote.rb
+++ app/models/party_vote.rb
@@ -1,3 +1,21 @@
 class PartyVote < Vote
 
+  after_save :expire_cached_pages
+
+  include ExpireCache
+
+  def expire_cached_pages
+    return unless is_file_cache?
+
+    uncache "/parliaments/#{Parliament.latest}.cache"
+    uncache "/parties/third_reading_and_negatived_votes.cache"
+    uncache "/parties/third_reading_and_negatived_votes.csv.cache"
+
+    list = Party.party_list
+    list.each do |party|
+      (list - party).each do |other_party|
+        uncache "/parties/#{party.id_name}/#{other_party.id_name}.cache"
+      end
+    end
+  end
 end
