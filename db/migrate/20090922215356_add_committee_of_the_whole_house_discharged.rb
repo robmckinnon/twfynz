fea4b3fa4d9ede8e6c0dbcd5016d98d43e9bb581
robmckinnon <rob@theyworkforyou.co.nz>
2009-09-22 22:56:45 +0100

    added committee_of_the_whole_house_discharged to bills

--- db/migrate/20090922215356_add_committee_of_the_whole_house_discharged.rb
+++ db/migrate/20090922215356_add_committee_of_the_whole_house_discharged.rb
@@ -1,7 +1,9 @@
 class AddCommitteeOfTheWholeHouseDischarged < ActiveRecord::Migration
   def self.up
+    add_column :bills, :committee_of_the_whole_house_discharged, :date
   end
 
   def self.down
+    remove_column :bills, :committee_of_the_whole_house_discharged
   end
 end
